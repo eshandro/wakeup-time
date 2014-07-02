d03baa6523cad7089f9f93beb61dbd2af9930cf9
eshandro <erik843@gmail.com>
2014-07-01 21:37:28 -0600

    Still building clock shell

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).on('ready', function() {
 	
 	var innershell = $('<div class="inner-shell"></div>');
 
-	var labels = $('<div class="labels"></div>');
+	var labels = $('<div class="labels"><h4>PM</h4><h4>AUTO</h4></div>');
 
 	var clockScreen = $('<div class="clockscreen"></div>');
 
@@ -12,12 +12,14 @@ $(document).on('ready', function() {
 
 	var clockText = $('<div class="clocktext"></div>');
 
-	var stations = $('<div class="stations"></div>');
+	var stations = $('<div class="stations"><p>AM 53 60 70 90 110 140 170</p><p>FM 88 92 96 102 106 108</p></div>');
 
 $('.container').prepend(outershell);
 $(outershell).prepend(innershell);
 $(innershell).prepend(clockScreen);
 $(innershell).prepend(labels);
+$(innershell).prepend(stations);
+
 
 
 });
