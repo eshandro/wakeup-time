6862f1df76cdfeefb202e85420dba95e8fddca92
Erik Shandrowski <erik843@gmail.com>
2014-07-02 12:35:23 -0600

    Mostly done with working clock

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
+var date = new Date;
+
+var hours = date.getHours();
+
+var minutes = date.getMinutes();
+
+var setTimeDisplay = function() {
+	if (hours > 12) {
+		var displayHour = (hours - 12);
+		var ampm = "PM";
+	}
+	else {
+		var displayHour = hours;
+		var ampm = "AM";
+	}
+	var displayTime = displayHour + ":" + minutes;
+	$('.clocktext-time').html(displayTime);
+	return displayTime;
+}
+
+
+
+var setAmPm = function() {
+	if (hours >= 12) {
+		$('.indicator').css('top', '20px');
+		}
+}
+
+
+
+
+
 $(document).on('ready', function() {
-  
+ 
+ 	setTimeDisplay();
+
+ 	window.setInterval(function(){setTimeDisplay()}, 60000);
+
 	var outershell = $('<div class="outer-shell"></div>');
 	
 	var innershell = $('<div class="inner-shell"></div>');
@@ -10,28 +46,28 @@ $(document).on('ready', function() {
 
 	var indicator = $('<div class="indicator"</div>');
 
-	var clockTextHour = $('<div class="clocktext-hour">12</div>');
+	var clockTextTime = $('<div class="clocktext-time">' + setTimeDisplay() + '</div>');
+	// var clockTextHour = $('<div class="clocktext-hour">12</div>');
 
-	var clockTextMinutes = $('<div class="clocktext-minutes">12</div>');
+	// var clockTextMinutes = $('<div class="clocktext-minutes">12</div>');
 
-	var clockTextDivider1 = $('<div class="clocktext-divider-1"></div>');
+	// // var clockTextDivider1 = $('<div class="clocktext-divider-1"></div>');
 
-	var clockTextDivider2 = $('<div class="clocktext-divider-2"></div>');
+	// // var clockTextDivider2 = $('<div class="clocktext-divider-2"></div>');
 
 	var stations = $('<div class="stations"><p>AM 53 60 70 90 110 140 170</p><p>FM 88 92 96 102 106 108</p></div>');
 
 
-$('.container').prepend(outershell);
-$(outershell).prepend(innershell);
-$(innershell).prepend(clockScreen);
-$(innershell).prepend(labels);
-$(innershell).prepend(stations);
-$(clockScreen).append(indicator);
-$(clockScreen).append(clockTextHour);
-$(clockScreen).append(clockTextDivider1);
-$(clockScreen).append(clockTextDivider2);
-$(clockScreen).append(clockTextMinutes);
 
+	$('.container').prepend(outershell);
+	$(outershell).prepend(innershell);
+	$(innershell).prepend(clockScreen);
+	$(innershell).prepend(labels);
+	$(innershell).prepend(stations);
+	$(clockScreen).append(indicator);
+	$(clockScreen).append(clockTextTime);
+
+ 	setAmPm();
 
 
 });
