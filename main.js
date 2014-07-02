a7d4f980e6b418af6a27b0c605f95891ae3bd9cd
Erik Shandrowski <erik843@gmail.com>
2014-07-02 10:46:54 -0600

    Done besides bonus

--- main.js
+++ main.js
@@ -10,15 +10,27 @@ $(document).on('ready', function() {
 
 	var indicator = $('<div class="indicator"</div>');
 
-	var clockText = $('<div class="clocktext"></div>');
+	var clockTextHour = $('<div class="clocktext-hour">12</div>');
+
+	var clockTextMinutes = $('<div class="clocktext-minutes">12</div>');
+
+	var clockTextDivider1 = $('<div class="clocktext-divider-1"></div>');
+
+	var clockTextDivider2 = $('<div class="clocktext-divider-2"></div>');
 
 	var stations = $('<div class="stations"><p>AM 53 60 70 90 110 140 170</p><p>FM 88 92 96 102 106 108</p></div>');
+
 
 $('.container').prepend(outershell);
 $(outershell).prepend(innershell);
 $(innershell).prepend(clockScreen);
 $(innershell).prepend(labels);
 $(innershell).prepend(stations);
+$(clockScreen).append(indicator);
+$(clockScreen).append(clockTextHour);
+$(clockScreen).append(clockTextDivider1);
+$(clockScreen).append(clockTextDivider2);
+$(clockScreen).append(clockTextMinutes);
 
 
 
